3b1a37fa3c28a03312c6389c337080e45ce80994
unknown <nemo5510@naver.com>
2020-08-26 01:16:50 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: process.env.MONGO_URI,
+};
+// Heruku에서의 이름과 같아야한다.
--- index.js
+++ index.js
@@ -2,21 +2,44 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// application/json
+app.use(bodyParser.json());
 
 const mongoose = require("mongoose");
 mongoose
-  .connect(
-    "mongodb+srv://nemo5510:1q2w3e4r!!@boilerplate.p62t9.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("MongoDB Connected..."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) =>
+  res.send("Hello World! ~~~~ 새해 복 많이 받으세요~")
+);
 
+// 회원가입을 위한 Route
+app.post("/register", (req, res) => {
+  // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
+  // 그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    }); // 몽고DB 메소드
+  });
+});
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//  서버 만드는 것
